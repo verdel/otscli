44e4f60224d280f37e5810462c83812eeec64abc
Vadim Aleksandrov <valeksandrov@me.com>
2017-06-13 15:12:58 +0300

    Some cosmetic change

--- otscli/otscli.py
+++ otscli/otscli.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 import requests
+import argparse
+import sys
 # Disable HTTPS verification warnings.
 from requests.packages import urllib3
 urllib3.disable_warnings()
-import argparse
-import sys
+
 
 def cli():
     parser = argparse.ArgumentParser(description='OTS console client')
-    
     parser.add_argument('-d', nargs='?', default='https://pwd.rk.local', help='ots service url (default: https://pwd.rk.local)', metavar='url', dest='ots_url')
     parser.add_argument('-s', required=True, help='the secret value which is encrypted before being stored.', metavar='secret', dest='ots_secret')
     parser.add_argument('-p', required=False, help='a string that the recipient must know to view the secret.', metavar='passphrase', dest='ots_passphrase')
@@ -20,11 +20,12 @@ def cli():
 
     return parser.parse_args()
 
+
 def client():
     args = cli()
     ots_data = dict(secret=args.ots_secret)
     if args.ots_passphrase:
-        ots_data.update({'passphrase':args.ots_passphrase})
+        ots_data.update({'passphrase': args.ots_passphrase})
     url = "{}/api/v1/share".format(args.ots_url)
     r = requests.post(url, data=ots_data, verify=False)
     print("{}/secret/{}" .format(args.ots_url, r.json()["secret_key"]))
